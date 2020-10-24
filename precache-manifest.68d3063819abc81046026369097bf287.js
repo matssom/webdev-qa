5a09f6a5c6a6569b4753df93a762de188534a1b4
Mats Sommervold <mats.sommervold@gmail.com>
2020-10-24 16:03:06 +0200

    Updates

---
+++ precache-manifest.68d3063819abc81046026369097bf287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac77270cafdb543fcfd1d42900210e9a",
+    "url": "/webdev-qa/index.html"
+  },
+  {
+    "revision": "13792ddb008bbb3ae0ba",
+    "url": "/webdev-qa/static/css/main.54811231.chunk.css"
+  },
+  {
+    "revision": "0a18d747a701190b98fc",
+    "url": "/webdev-qa/static/js/2.3692e76f.chunk.js"
+  },
+  {
+    "revision": "c2e52282d033fca86fa91aac36af9388",
+    "url": "/webdev-qa/static/js/2.3692e76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13792ddb008bbb3ae0ba",
+    "url": "/webdev-qa/static/js/main.713c3467.chunk.js"
+  },
+  {
+    "revision": "567d688b9b10076109da",
+    "url": "/webdev-qa/static/js/runtime-main.8e30679e.js"
+  }
+]);
